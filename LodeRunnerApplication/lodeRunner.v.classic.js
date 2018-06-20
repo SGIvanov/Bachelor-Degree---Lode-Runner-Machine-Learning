e4b56b969329f86ee2deee004cccce46d1770ba7
Ivanov Silviu-Gabriel <silviu.ivanov@yahoo.com>
2018-06-21 01:55:25 +0300

    Repair + Licență

--- LodeRunnerApplication/lodeRunner.v.classic.js
+++ LodeRunnerApplication/lodeRunner.v.classic.js
@@ -1,5 +1,22 @@
-var classicData = ["H   # -##  ###$   -S#HH# # #HH  ##@ #   ###  # S#HH # ## H  ##@ # #   $  0   HH@  - ##  # -$ # ##$#    # #H ##@ ## ##  ###  #  H#0    H  ##-$#H#@$ $#$  -  H#    #H    -  #     ##  H  H# ##  H- $  H##         H  H$   # H  #- H-  - #  -  H- H $  # #     H #  @ H# H#H@#H $# # #   --H##$## H  H HH$H#$#  ## #@ HH @#   H@ H HH#H#@#      @  HH ##  #  H HH# #-#- -#  ## $H $ # #  H#HH#  -  # #  ## 0H @ # #  H$HH#  X###-    # # #@#   HH $HH#  #H # ##    "];
-
-//$.get("http://localhost:8498/api/Maps/Level", function (data) {
-//    classicData[0] = data;
-//});
+var classicData = [];
+//classicData[0] = "                  S         " +
+//    "                  S         " +
+//    "#######H#######   S         " +
+//    "       H----------S    $    " +
+//    "       H    ##H   #######H##" +
+//    "       H    ##H          H  " +
+//    "     0 H    ##H        0 H  " +
+//    "##H#####    ########H#######" +
+//    "  H                 H       " +
+//    "  H                 H       " +
+//    "#########H##########H       " +
+//    "         H          H       " +
+//    "         H----------H   $   " +
+//    "    H######         #######H" +
+//    "    H         &  $ $$      H" +
+//    "############################";
+jQuery.ajaxSettings.async = false;
+$.get("http://localhost:8498/api/Maps/Level", function (data) {
+   classicData[0] = data;
+});
+jQuery.ajaxSettings.async = true;
